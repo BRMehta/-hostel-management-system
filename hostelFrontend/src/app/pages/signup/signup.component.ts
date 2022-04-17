81bd23545961a95fd72ba8e93f9e8bea0973fa2c
brm1996 <mehtabhargavr@gmail.com>
2022-04-17 10:09:59 +0530

    improved success or fail msg on signup page

--- hostelFrontend/src/app/pages/signup/signup.component.ts
+++ hostelFrontend/src/app/pages/signup/signup.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { UserService } from 'src/app/service/user.service';
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-signup',
@@ -8,35 +10,50 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 })
 export class SignupComponent implements OnInit {
 
-  constructor(private snak:MatSnackBar) { }
+  constructor(private userSerice: UserService, private snack: MatSnackBar) { }
 
   //variable name=backend var name
-  public user={
+  public user = {
     userName: '',
     password: '',
     firstName: '',
     lastName: '',
     email: '',
-    phone: '',
+    phone: ''
   };
 
   ngOnInit(): void {
 
   };
 
-  formSubmit()
-  {
-      console.log(this.user);
-      if(this.user.userName==''|| this.user.userName==null ||
-         this.user.password==''|| this.user.password==null ||
-         this.user.firstName==''|| this.user.firstName==null ||
-         this.user.lastName==''|| this.user.lastName==null ||
-         this.user.email==''|| this.user.email==null ||
-         this.user.phone==''|| this.user.phone==null
-         )
-      {
-        this.snak.open("fields can not be empty!!","OK");
-        return;
+  formSubmit() {
+    console.log(this.user);
+    if (this.user.userName == '' || this.user.userName == null ||
+      this.user.password == '' || this.user.password == null ||
+      this.user.firstName == '' || this.user.firstName == null ||
+      this.user.lastName == '' || this.user.lastName == null ||
+      this.user.email == '' || this.user.email == null ||
+      this.user.phone == '' || this.user.phone == null
+    ) {
+      this.snack.open("fields can not be empty!!", "OK",{duration:3000});
+      return;
+    }
+
+    // addUser:userService
+    this.userSerice.addUser(this.user).subscribe(
+      (data:any) => {
+        //Success
+        console.log(data)
+        //alert('success')
+        //this.snack.open("Success!", "OK",{duration:3000});
+        //Swal.fire('Success!','user is registered','success');
+        Swal.fire('Success!','user id is ' + data.id,'success');
+      },
+      (error) => {
+        console.log(error);
+        //this.snack.open("Something went wrong!", "OK",{duration:3000});
+        Swal.fire('Error','Something went wrong!','error');
       }
+    )
   }
 }
