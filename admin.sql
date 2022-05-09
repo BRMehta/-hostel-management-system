9d7007b3513d5f22317a00629edc5b83d6e96ec0
brm1996 <mehtabhargavr@gmail.com>
2022-05-09 15:19:11 +0530

    Added admin.sql

---
+++ admin.sql
@@ -0,0 +1,3 @@
+INSERT INTO `role` VALUES (0,44,'ADMIN');
+INSERT INTO `user` VALUES (0,'hostel.iiitb1@gmail.com',_binary '','admin','admin','admin','9208193442','default.png','admin');
+INSERT INTO `user_role` VALUES (0,44,0);
